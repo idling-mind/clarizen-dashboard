d7d07ca1d11841e5d8bbe5f730619998330af7b9
Najeem Muhammed <najeem@gmail.com>
2018-05-06 07:59:06 +0530

    Added bootstrap page structure

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,5 +1,13 @@
 <template>
-  <page-header title='Clarizen Users' subtitle='Clarizen Users at GAI'></page-header>
+  <div class="page">
+    <div class="page-main">
+      <div class="my-3 my-md-5">
+        <div class="container-fluid">
+          <page-header title='Clarizen Users' subtitle='Clarizen Users at GAI'></page-header>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
