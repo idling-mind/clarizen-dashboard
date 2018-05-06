f8d3be276720a73e438276b060668ee1a99f6755
Najeem Muhammed <najeem@gmail.com>
2018-05-06 08:32:52 +0530

    Created CardBigProgress

---
+++ src/components/cards/CardBigProgress.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <div class="card-body text-center">
+      <div class="h5">{{ title }}</div>
+      <div class="display-4 font-weight-bold mb-4">{{ hilight }}</div>
+      <div class="progress progress-sm">
+          <div class="progress-bar bg-blue" :style="getProgress()"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardBigProgress',
+  props: {
+    hilight: String,
+    title: String,
+    progress: Number
+  },
+  methods: {
+    getProgress () {
+      var p = this.progress
+      if (p > 100) {
+        p = 100
+      } else if (p < 0) {
+        p = 0
+      }
+      return 'width: ' + p + '%'
+    }
+  }
+}
+</script>
