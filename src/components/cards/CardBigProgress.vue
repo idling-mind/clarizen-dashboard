fa8979bae9224565c174d48fea174747508b41e2
Najeem Muhammed <najeem@gmail.com>
2018-05-06 09:55:36 +0530

    Fixed alighnment in CardBigProgress

--- src/components/cards/CardBigProgress.vue
+++ src/components/cards/CardBigProgress.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="card">
-    <div class="card-body text-center">
-      <div class="h5">{{ title }}</div>
-      <div class="display-4 font-weight-bold mb-4">{{ hilight }}</div>
-      <div class="progress progress-sm">
-          <div :class="color" class="progress-bar" :style="getProgress()"></div>
+  <div class="col-lg-4 col-md-6 col-sm-12">
+    <div class="card">
+      <div class="card-body text-center">
+        <div class="h5">{{ title }}</div>
+        <div class="display-4 font-weight-bold mb-4">{{ hilight }}</div>
+        <div class="progress progress-sm">
+            <div :class="color" class="progress-bar" :style="getProgress()"></div>
+        </div>
       </div>
     </div>
   </div>
