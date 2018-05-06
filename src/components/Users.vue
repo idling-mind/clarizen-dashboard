a65c0e8838d4efd9db74f821906aa22c937cf241
Najeem Muhammed <najeem@gmail.com>
2018-05-06 08:32:36 +0530

    Added cardbigprogress to structure

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -4,6 +4,15 @@
       <div class="my-3 my-md-5">
         <div class="container-fluid">
           <page-header title='Clarizen Users' subtitle='Clarizen Users at GAI'></page-header>
+          <div class="row row-cards">
+            <div class="col-lg-8 col-md-6">
+              <div class="row">
+                <div class="col-lg-6">
+                  <card-big-progress title="Number of Users" hilight="106" :progress=100></card-big-progress>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -12,6 +21,7 @@
 
 <script>
 import PageHeader from './PageHeader.vue'
+import CardBigProgress from './cards/CardBigProgress.vue'
 
 export default {
   name: 'Users',
@@ -21,7 +31,8 @@ export default {
     }
   },
   components: {
-    PageHeader
+    PageHeader,
+    CardBigProgress
   }
 }
 </script>
