464dd05db0c9a9064deeb7c298858f64d9ae0dac
Muhammed Najeem <yy53393@aeroes.internal>
2018-05-07 13:03:39 +0530

    Updated with more cards

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -5,9 +5,10 @@
         <div class="container-fluid">
           <page-header title='Clarizen Users' subtitle='Clarizen Users at GAI'></page-header>
           <div class="row row-cards">
-            <card-big-progress title="Number of Users" hilight="106" :progress=100 color="bg-blue"></card-big-progress>
-            <card-small-progress title="Not Logged in a Month" hilight="28" :progress=28 color="bg-red"></card-small-progress>
-            <card-small-progress title="Logged in today" hilight="21" :progress=21 color="bg-green"></card-small-progress>
+            <card-small-progress title="Number of Users" :hilight="noOfUsers" :progress=100 color="bg-blue"></card-small-progress>
+            <card-small-progress title="Not logged ever" :hilight="noOfNonLoggers.count" :progress="noOfNonLoggers.percent" color="bg-red"></card-small-progress>
+            <card-small-progress title="Not Logged in a Month" :hilight="noOfMonthLoggers.count" :progress="noOfMonthLoggers.count" color="bg-red"></card-small-progress>
+            <card-small-progress title="Logged in Today" :hilight="noOfTodayLoggers.count" :progress="noOfTodayLoggers.count" color="bg-green"></card-small-progress>
           </div>
         </div>
       </div>
@@ -16,6 +17,8 @@
 </template>
 
 <script>
+import _ from 'lodash'
+import clapi from '../clarizen/clapi.js'
 import PageHeader from './PageHeader.vue'
 import CardBigProgress from './cards/CardBigProgress.vue'
 import CardSmallProgress from './cards/CardSmallProgress.vue'
@@ -24,13 +27,90 @@ export default {
   name: 'Users',
   data () {
     return {
-      msg: 'Clarizen User Statistics'
+      users: []
     }
   },
   components: {
     PageHeader,
     CardBigProgress,
     CardSmallProgress
+  },
+  methods: {
+    getUsers () {
+      console.log('Initiated getUsers')
+      clapi.post('data/relationQuery', '{"entityId":"/DiscussionGroup/1jipe5uebj67scpwpqnk7vgn5502","relationName":"GroupMembers","fields":["FirstName", "Lastname", "Username", "Lastlogin", "LicenseType.Name", "State.Name"],"where":"State IN (\'Active\',\'Draft\')","paging":{"from":0, "limit": 500}}'
+      ).then(response => {
+        this.users = response.data.entities
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  },
+  mounted () {
+    this.getUsers()
+  },
+  computed: {
+    noOfUsers: function () {
+      return this.users.length
+    },
+    noOfNonLoggers: function () {
+      var x = _.countBy(this.users, function (item) {
+        return item.Lastlogin == null
+      })
+      return {
+        count: x.true,
+        percent: x.true / this.noOfUsers
+      }
+    },
+    noOfMonthLoggers: function () {
+      var now = Date.now()
+      var x = _.countBy(this.users, function (item) {
+        var daydiff = (now - Date.parse(item.Lastlogin)) / (1000 * 60 * 60 * 24)
+        return daydiff > 30
+      })
+      return {
+        count: x.true,
+        percent: x.true / this.noOfUsers
+      }
+    },
+    noOfTodayLoggers: function () {
+      var now = Date.now()
+      var x = _.countBy(this.users, function (item) {
+        var daydiff = (now - Date.parse(item.Lastlogin)) / (1000 * 60 * 60 * 24)
+        return daydiff < 1
+      })
+      return {
+        count: x.true,
+        percent: x.true / this.noOfUsers
+      }
+    },
+    noOfPMs: function () {
+      var x = _.countBy(this.users, function (item) {
+        return item.LicenseType.Name === 'Full'
+      })
+      return {
+        count: x.true,
+        percent: x.true / this.noOfUsers
+      }
+    },
+    noOfTMs: function () {
+      var x = _.countBy(this.users, function (item) {
+        return item.LicenseType.Name === 'TeamMember'
+      })
+      return {
+        count: x.true,
+        percent: x.true / this.noOfUsers
+      }
+    },
+    groupLastLogin: function () {
+      var now = Date.now()
+      var x = _.countBy(this.users, function (item) {
+        var daydiff = (now - Date.parse(item.Lastlogin)) / (1000 * 60 * 60 * 24)
+        return Math.floor(daydiff)
+      })
+      console.log(x)
+      return x
+    }
   }
 }
 </script>
