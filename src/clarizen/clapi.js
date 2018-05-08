c550ba431c622c4ce6d9a295c576f7f81906a100
Muhammed Najeem <yy53393@aeroes.internal>
2018-05-08 12:42:49 +0530

    Changed api key

--- src/clarizen/clapi.js
+++ src/clarizen/clapi.js
@@ -6,6 +6,6 @@ export default axios.create({
   async: true,
   crossdomain: true,
   headers: {
-    'Authorization': 'Session e542abcb-db3b-4409-828d-6a1fa1622ac8_25446405'
+    'Authorization': 'Session 3c4ed5c3-f2d0-4a63-aa34-31be0507749c_25446405'
   }
 })
