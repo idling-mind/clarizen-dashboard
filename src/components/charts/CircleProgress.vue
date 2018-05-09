6054477f2384fba66366d2a6b3bc2f628ff69920
Muhammed Najeem <yy53393@aeroes.internal>
2018-05-09 14:50:54 +0530

    Added circle progress

---
+++ src/components/charts/CircleProgress.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">{{ title }}</h3>
+    </div>
+    <div class="card-body text-center">
+      <div class="c3">
+        <radial-progress-bar :diameter="300"
+                              :completed-steps=Math.floor(number)
+                              :total-steps=100
+                              :startColor="statuscolor()"
+                              :stopColor="statuscolor()"
+                              :strokeWidth=25
+                              innerStrokeColor="#dddddd">
+          <div class="h1">{{ Math.floor(number) }}%</div>
+        </radial-progress-bar>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RadialProgressBar from 'vue-radial-progress'
+import tabler from '../../assets/js/Colors.js'
+
+export default {
+  name: 'RadialProgress',
+  props: {
+    title: String,
+    number: Number,
+    status: String
+  },
+  methods: {
+    statuscolor () {
+      if (this.status === 'On Track') {
+        return tabler.colors.green
+      } else if (this.status === 'At Risk') {
+        return tabler.colors.orange
+      } else if (this.status === 'Off Track') {
+        return tabler.colors.red
+      }
+    }
+  },
+  components: {
+    RadialProgressBar
+  }
+}
+</script>
+
+<style scoped>
+  .radial-progress-container {
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
