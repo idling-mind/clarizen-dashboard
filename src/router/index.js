82e4353453ce287c3fab194662041c14f2c6bf25
Muhammed Najeem <yy53393@aeroes.internal>
2018-05-09 14:55:23 +0530

    Added strategy matrix page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Users from '@/components/Users'
+import StrategyMatrix from '@/components/StrategyMatrix'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Users',
       component: Users
+    },
+    {
+      path: '/strategy',
+      name: 'StrategyMatrix',
+      component: StrategyMatrix
     }
   ]
 })
